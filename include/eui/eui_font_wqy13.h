79fd496a05213f4289a2884af77c14cd80729ecc
solosky <solosky772@qq.com>
2026-05-22 23:06:20 +0800

    feat: 内置 wqy13 中文字体 eui_font_wqy13

---
+++ include/eui/eui_font_wqy13.h
@@ -0,0 +1,11 @@
+#ifndef EUI_FONT_WQY13_H
+#define EUI_FONT_WQY13_H
+
+#include <stdint.h>
+#include "eui/eui_types.h"
+#include "eui/eui_font.h"
+
+extern const uint8_t eui_font_wqy13_data[];
+extern const eui_font_t eui_font_wqy13;
+
+#endif
